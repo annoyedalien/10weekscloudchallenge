e23a93bad7907ef1c2a510f551642de6d56520e5
annoyedalien <wishmaster@DESKTOP-I0ACG29.>
2024-12-12 19:51:46 +0800

    update web_setup.sh

--- Week2/web_setup.sh
+++ Week2/web_setup.sh
@@ -2,6 +2,11 @@
 
 sudo apt update -y
 
+
+echo "----------------------------------------------------------"
+echo "|          Updating and Installing dependencies           |"
+echo "----------------------------------------------------------"
+
 sudo apt install apache2 -y
 
 curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - &&\
@@ -16,3 +21,23 @@ corepack enable
 corepack prepare yarn@stable --activate --yes
 
 sudo yarn global add pm2
+
+
+echo "----------------------------------------------------------"
+echo "| Do you want make changes to config.js file?             |"
+echo "----------------------------------------------------------"
+
+read -p "yes/no: " ans
+
+if [[ "$ans" == "no" ]]; then
+ echo "Great So, make Changes to line 58 replace it with intenal-loadbalancer-dns "
+elif [[ "$ans" == "yes" ]]; then
+
+sudo nano /client/src/pages/config.js
+cd client
+
+sudo npm install
+npm run build
+sudo cp -r build/* /var/www/html
+
+
