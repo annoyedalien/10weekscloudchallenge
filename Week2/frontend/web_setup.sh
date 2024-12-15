c02535df9d999f3c8fddfd77defb7f89dc386b72
annoyedalien <wishmaster@DESKTOP-I0ACG29.>
2024-12-15 13:10:02 +0800

    update web_setup.sh

--- Week2/frontend/web_setup.sh
+++ Week2/frontend/web_setup.sh
@@ -1,7 +1,9 @@
-#!bin/bash
+#!/bin/bash
 sudo apt-get update
 sudo apt install npm
 npx create-react-app -y "might need to update"
-npm install axios react-dom react-scripts
+npm install axios 
+npm install react-dom 
+npm install react-scripts
 sudo apt-install nginx -y
 
